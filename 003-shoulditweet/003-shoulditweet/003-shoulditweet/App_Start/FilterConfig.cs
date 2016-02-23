aa07b6beb46d5aee89f0aec7fb4125abc9da291c
Sunny Wijeratne <sunny.wijeratne@gmail.com>
2016-02-23 21:32:36 +1000

    created project for 003

---
+++ 003-shoulditweet/003-shoulditweet/003-shoulditweet/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _003_shoulditweet
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
